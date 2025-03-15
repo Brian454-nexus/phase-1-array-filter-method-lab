a6668def217c9cb35570bef23f7cc6c601eec207
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-03-15 12:10:11 +0300

    Added Array Filter

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+// 1️⃣ Find drivers that match the name (case insensitive)
+function findMatching(array, value) {
+    return array.filter((element) => element.toLowerCase() === value.toLowerCase());
+}
+
+// 2️⃣ Find drivers whose names *start with* the given letters
+function fuzzyMatch(array, value) {
+    return array.filter((element) => element.startsWith(value));
+}
+
+// 3️⃣ Find drivers with an exact name match inside an object
+function matchName(array, value) {
+    return array.filter((element) => element.name === value);
+}
+
+
